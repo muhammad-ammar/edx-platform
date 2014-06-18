20128e125bfc70a716137324b518077baecd7be2
Christine Lytwynec <clytwynec@edx.org>
2014-06-18 16:41:48 -0400

    Deprecating acceptance_tests from rake to paver

--- pavelib/utils/test/suites/__init__.py
+++ pavelib/utils/test/suites/__init__.py
@@ -6,3 +6,4 @@ from .nose_suite import NoseTestSuite, SystemTestSuite, LibTestSuite
 from .python_suite import PythonTestSuite
 from .js_suite import JsTestSuite
 from .i18n_suite import I18nTestSuite
+from .acceptance_suite import AcceptanceTestSuite
