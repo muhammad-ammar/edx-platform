dad668df1a405dfff08d1b1447ea76477a9afa5c
Alexander Kryklia <kryklia@gmail.com>
2014-06-19 12:23:01 +0300

    Add logs for debugging. Will be removed later.

--- common/djangoapps/geoinfo/middleware.py
+++ common/djangoapps/geoinfo/middleware.py
@@ -31,9 +31,17 @@ class CountryMiddleware(object):
         """
         new_ip_address = get_ip(request)
         old_ip_address = request.session.get('ip_address', None)
+        log.warning('Old IP: %s', old_ip_address)
+        log.warning('New IP: %s', new_ip_address)
 
         if new_ip_address != old_ip_address:
-            country_code = pygeoip.GeoIP(settings.GEOIP_PATH).country_code_by_addr(new_ip_address)
+            try:
+                country_code = pygeoip.GeoIP(settings.GEOIP_PATH).country_code_by_addr(new_ip_address)
+            except Exception as exc:
+                log.warning('pygeoip exception: %s', str(exc))
+                log.warning('geoippath is %s', settings.GEOIP_PATH)
             request.session['country_code'] = country_code
             request.session['ip_address'] = new_ip_address
-            log.debug('Country code for IP: %s is set to %s', new_ip_address, country_code)
+            log.warning('Country code for IP: %s is set to %s', new_ip_address, country_code)
+        else:
+            log.warning('Geoinfo: no change.')
