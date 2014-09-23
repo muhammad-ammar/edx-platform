01fd0f1158f4a1fe1f3dc710e2fe37c6a85d1b3e
Muhammad Ammar <mammar@edx.org>
2014-09-23 18:01:44 +0500

    Yet another attempt to combine bok-choy coverage

--- scripts/post_worker.py
+++ scripts/post_worker.py
@@ -6,6 +6,12 @@ full_path = os.path.realpath(__file__)
 source_dir = full_path.replace("scripts/post_worker.py", "reports/")
 output_filename = full_path.replace("post_worker.py", "reports.tar.gz")
 
+session_path = os.path.join(
+    os.environ['HOME'],
+    'results',
+    os.environ['TDDIUM_SESSION_ID'],
+    'session')
+
 print "source dir:", source_dir
 
 count = 0
@@ -15,16 +21,15 @@ with tarfile.open(output_filename, "w:gz") as tar:
     for (path, dirs, files) in os.walk(source_dir):
         if len(files) > 0:
             print "tarring:", path
+            
+            # copy bok-choy .coverage.* files for combining later
+	    if os.path.basename(path) == 'bok_choy':
+                copy_bokchoy_coverage_data(path, files)
+                
             tar.add(path, arcname=os.path.basename(path))
             count += 1
 
 tar.close()
-
-session_path = os.path.join(
-    os.environ['HOME'],
-    'results',
-    os.environ['TDDIUM_SESSION_ID'],
-    'session')
 
 file_dest = os.path.join(session_path, 'reports.tar.gz')
 
@@ -45,3 +50,17 @@ for (path, dirs, files) in os.walk('test_root/log'):
             shutil.copyfile(filepath, destpath)
 
 print 'TDDIUM_SESSION_ID:', os.environ['TDDIUM_SESSION_ID']
+
+
+def copy_bokchoy_coverage_data(source_path, files):
+
+    destination_path = os.path.join(session_path, 'bok_choy')
+
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path)
+    
+    for f in files:
+        if f.startswith('.coverage.'):
+            file_path = os.path.join(source_path, f)
+            shutil.copyfile(file_path, destination_path)
+
