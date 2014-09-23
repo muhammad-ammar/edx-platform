a5eb16447994e71d92b61df04762af31a2ffed51
Muhammad Ammar <mammar@edx.org>
2014-09-23 15:19:32 +0500

    more debugging

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,7 +2,7 @@
 
 current_path=`pwd`
 bok_choy_reports_path=$current_path/reports/bok_choy
-bok_choy_cov_data=$bok_choy_reports_path/.coverage
+bok_choy_cov_data=$bok_choy_reports_path/.coverage*
 dest_path=$HOME/results/$TDDIUM_SESSION_ID/session/bok_choy
 
 mkdir -p $dest_path 
@@ -14,15 +14,15 @@ echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 case $1 in
    "shard1")	
 	echo "Copying Bok-Choy Shard1 Coverage Data to "$dest_path
-	cp -f $bok_choy_cov_data $dest_path/.coverage.1
+	cp -f $bok_choy_cov_data $dest_path/
 	;;
    "shard2")
 	echo "Copying Bok-Choy Shard2 Coverage Data to "$dest_path
-	cp -f $bok_choy_cov_data $dest_path/.coverage.2
+	cp -f $bok_choy_cov_data $dest_path/
 	;;
    "shard3")
 	echo "Copying Bok-Choy Shard3 Coverage Data to "$dest_path
-	cp -f $bok_choy_cov_data $dest_path/.coverage.3
+	cp -f $bok_choy_cov_data $dest_path/
 	;;
    *)
 	echo "Invalid Bok-Choy Shard Value!";;
